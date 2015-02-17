54650b71e3ea2f94826fffa308cbea92a1186e8a
Dan Varga <dvarga@redhat.com>
2015-02-17 12:30:20 -0500

    Remove local log viewing, non-deterministic in HA environment

--- src/redhat_access_plugin_openstack/logs/urls.py
+++ src/redhat_access_plugin_openstack/logs/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import IndexView, LogView, LocalLogView, logs
+from .views import IndexView, LogView
 
 urlpatterns = patterns(
     '',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^view$', LogView.as_view(), name='view'),
-    url(r'^viewlocal$', LocalLogView.as_view(), name='viewlocal'),
-    url(r'^logs$', logs, name='logs'),
 )
